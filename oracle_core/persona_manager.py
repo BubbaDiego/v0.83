f4dda903e875c3f973878df66244545770439717
BubbaDiego <106400961+BubbaDiego@users.noreply.github.com>
2025-05-27 18:17:32 -0700

    Fix oracle strategy retrieval and persona loading

--- oracle_core/persona_manager.py
+++ oracle_core/persona_manager.py
@@ -1,7 +1,6 @@
 import json
 import os
 
-from pathlib import Path
 from typing import Dict, Iterable
 
 
@@ -13,6 +12,8 @@ class Persona:
         self.name = data.get("name", "")
         self.description = data.get("description", "")
         self.modifiers = data.get("modifiers", {})
+        self.strategy_weights = data.get("strategy_weights", {})
+        self.instructions = data.get("instructions", "")
 
 class PersonaManager:
     """Load and manage personas."""
@@ -29,8 +30,6 @@ class PersonaManager:
             if fname.endswith(".json"):
                 self.load_from_file(os.path.join(self.base_dir, fname))
 
-        self.strategy_weights = data.get("strategy_weights", {})
-        self.instructions = data.get("instructions", "")
 
 
 
